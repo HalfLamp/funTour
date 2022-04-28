629cada173cffda141352d50b0dfad9170f428a2
HalfLamp <caizhiyuan826@163.com>
2022-04-28 10:39:24 +0800

    修改dockerfile逻辑

---
+++ gateway/src/main/java/com/cai/funtour/config/Users.java
@@ -0,0 +1,13 @@
+package com.cai.funtour.config;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ：caizhiyuan
+ * @date ：Created in 2022/4/26 11:23
+ * @description：ceshi
+ */
+@Component
+public class Users {
+
+}
--- gateway/src/test/java/com/cai/funtour/GatewayApplicationTests.java
+++ gateway/src/test/java/com/cai/funtour/GatewayApplicationTests.java
@@ -1,13 +1,19 @@
 package com.cai.funtour;
 
+import com.cai.funtour.config.Users;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = GatewayApplicationTests.class)
+@SpringBootTest(classes = GatewayApplication.class)
 class GatewayApplicationTests {
+
+    @Autowired
+    Users users;
 
     @Test
     void contextLoads() {
+        System.out.println(users);
     }
 
 }

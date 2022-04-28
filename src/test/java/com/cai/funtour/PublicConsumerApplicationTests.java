b831749eb34a71e91c36ffcb492584810a5d0359
HalfLamp <caizhiyuan826@163.com>
2022-04-28 10:58:28 +0800

    1.初始化新分支

---
+++ src/test/java/com/cai/funtour/PublicConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cai.funtour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PublicConsumerApplication.class)
+class PublicConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

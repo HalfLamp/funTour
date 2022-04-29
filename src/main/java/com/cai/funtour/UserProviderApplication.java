3cd1380aaab9229dabbf7c5b8e5d4cb541aa58d9
HalfLamp <caizhiyuan826@163.com>
2022-04-29 17:09:28 +0800

    1.修改配置

--- src/main/java/com/cai/funtour/UserProviderApplication.java
+++ src/main/java/com/cai/funtour/UserProviderApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class UserProviderApplication {
 
     public static void main(String[] args) {
-            SpringApplication.run(UserProviderApplication.class, args);
+        SpringApplication.run(UserProviderApplication.class, args);
     }
 
 }

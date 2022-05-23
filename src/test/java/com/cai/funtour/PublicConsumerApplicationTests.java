cc698e8364f45d3216f97b22f1c4692c5b9a5e80
HalfLamp <caizhiyuan826@163.com>
2022-05-23 14:51:40 +0800

    修改RSA

--- src/test/java/com/cai/funtour/PublicConsumerApplicationTests.java
+++ src/test/java/com/cai/funtour/PublicConsumerApplicationTests.java
@@ -1,13 +1,29 @@
 package com.cai.funtour;
 
+import com.alibaba.fastjson.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @SpringBootTest(classes = PublicConsumerApplication.class)
 class PublicConsumerApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(1024);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+
+        System.out.println(publicKey.getPublicExponent());
+        System.out.println(JSONObject.toJSONString(privateKey));
+
     }
 
 }

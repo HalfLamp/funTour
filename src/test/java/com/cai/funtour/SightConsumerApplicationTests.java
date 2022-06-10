31ed94704607a78186bab8d3121e1facb37d076a
HalfLamp <caizhiyuan826@163.com>
2022-06-10 16:56:07 +0800

    1.添加入口函数

---
+++ src/test/java/com/cai/funtour/SightConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cai.funtour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SightConsumerApplicationTests.class)
+class SightConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

89b0f1e18b5b7cd5da9de2f43c5d26811b5012be
HalfLamp <caizhiyuan826@163.com>
2022-06-27 15:20:38 +0800

    1.全局异常捕获增加错误类型

--- src/main/java/com/cai/funtour/config/ExceptionHandlerConfig.java
+++ src/main/java/com/cai/funtour/config/ExceptionHandlerConfig.java
@@ -21,7 +21,7 @@ public class ExceptionHandlerConfig {
     @ResponseBody
     public Result exceptionHandler(Exception exception){
         log.error("请求发生错误,错误原因：{}\n错误类型：{}；错误信息: {}", exception.getCause(), exception.getClass().getName(), exception.getMessage());
-        Arrays.stream(exception.getStackTrace()).forEach(item -> {
+        Arrays.stream(exception.getStackTrace()).limit(5).forEach(item -> {
             log.error("错误调用栈： {}", item.getClassName() + ":" + item.getMethodName() + ":" + item.getLineNumber());
         });
         return Result.error(500, "未知异常");

1a807e5b9b26aabf3f8b339ac4cafd5103cce016
HalfLamp <caizhiyuan826@163.com>
2022-06-30 14:18:12 +0800

    1.ErrorResponse类

--- src/main/java/com/cai/funtour/config/ExceptionHandlerConfig.java
+++ src/main/java/com/cai/funtour/config/ExceptionHandlerConfig.java
@@ -1,6 +1,7 @@
 package com.cai.funtour.config;
 
 import com.cai.funtour.pojo.Result;
+import com.cai.funtour.tools.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,6 +25,6 @@ public class ExceptionHandlerConfig {
         Arrays.stream(exception.getStackTrace()).limit(5).forEach(item -> {
             log.error("错误调用栈： {}", item.getClassName() + ":" + item.getMethodName() + ":" + item.getLineNumber());
         });
-        return Result.error(500, "未知异常");
+        return ErrorResponse.getErrorResult(exception.getClass().getName());
     }
 }

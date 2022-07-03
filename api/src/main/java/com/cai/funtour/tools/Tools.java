4395b0186abbfbbf93a782be62eb3c088e494f37
HalfLamp <caizhiyuan826@163.com>
2022-07-03 22:39:18 +0800

    1.增加kafka topics

--- api/src/main/java/com/cai/funtour/tools/Tools.java
+++ api/src/main/java/com/cai/funtour/tools/Tools.java
@@ -1,18 +1,22 @@
 package com.cai.funtour.tools;
 
-import java.util.Set;
-
 /**
  * @author ：caizhiyuan
  * @date ：Created in 2022/4/21 15:16
  * @description：字典类
  */
 public class Tools {
+    // system params
     public static final String TRACE_ID = "traceId";
     public static final String TOKEN = "token";
     public static final String JWT_IISUSER = "funtour";
     public static final String JWT_SUBJECT_USER = "normal_user";
     public static final String JWT_SUBJECT_SYSTEM = "manager_user";
 
+    // redis keys
     public static final String CACHE_TOKEN = "FUNTOUR:USER:TOKEN_";
+
+    // kafka topics name
+    public static final String KAFKA_TOPIC_PREFERENCE = "funtour_sight_preference_r1p2";
+    public static final String KAFKA_TOPIC_HOTSIGHT = "funtour_sight_hot_r1p2";
 }

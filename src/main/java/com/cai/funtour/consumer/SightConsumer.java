2798084c1419e81e034720bbc5b1339be601a7d2
HalfLamp <caizhiyuan826@163.com>
2022-07-09 14:53:47 +0800

    1.修改测试类，避免导致测试不通过

--- src/main/java/com/cai/funtour/consumer/SightConsumer.java
+++ src/main/java/com/cai/funtour/consumer/SightConsumer.java
@@ -1,7 +1,6 @@
 package com.cai.funtour.consumer;
 
 import com.alibaba.fastjson.JSON;
-import com.cai.funtour.mapper.LabelMapper;
 import com.cai.funtour.po.Label;
 import com.cai.funtour.service.LabelService;
 import com.cai.funtour.tools.LabelType;
@@ -15,7 +14,6 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
